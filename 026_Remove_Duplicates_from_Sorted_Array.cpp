4178f9f932ed9f8070f1629786953f4ee0c2dbf1
Tingwei Jen <tingweijen@gmail.com>
2017-09-26 21:49:28 +0800

    Update 026_Remove_Duplicates_from_Sorted_Array.cpp

--- 026_Remove_Duplicates_from_Sorted_Array.cpp
+++ 026_Remove_Duplicates_from_Sorted_Array.cpp
@@ -12,17 +12,12 @@ public:
         for (int i=1;i<nums.size();i++){
             
             if(nums[count] !=nums[i]){           // curent != next  看 下一個是不是等於現在這個
-                count++;                         // 如果不等於 --> count +1, 然後把nums[i] 放進來
+                count++;                         // 如果不等於 --> count +1, 然後把nums[i] 放進來 把原先的取代掉
                 nums[count] = nums[i];
             }
         }
         
         count = count +1;     //加上最一開始的數字
-        
-        return count;
-        
-        
-        
-        
+        return count;        
     }
 };
