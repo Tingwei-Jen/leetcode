c31298d3043795f09509916c9c2dedc45dffea71
Tingwei Jen <tingweijen@gmail.com>
2017-09-28 18:26:56 +0800

    Update 078_Subsets.cpp

--- leetcode/078_Subsets.cpp
+++ leetcode/078_Subsets.cpp
@@ -8,20 +8,38 @@ public:
         if(nums.size()==0) return res;
         
         vector<int> cur;
-        addnumberDFS(res, nums, 0, cur);
-        
+        //addnumberDFSmethod1(res, nums, 0, cur);
+        addnumberDFSmethod2(res, nums, 0, cur);
         return res;
         
     }
     
-    void addnumberDFS(vector<vector<int>> &res, vector<int> &nums, int index, vector<int> cur){
+    void addnumberDFSmethod1(vector<vector<int>> &res, vector<int> &nums, int index, vector<int> cur){
         
         res.push_back(cur);                               //遞迴這個動作
         
         for(int i=index; i<nums.size();i++){    
             cur.push_back(nums[i]);
-            addnumberDFS(res, nums, i+1, cur);            // i + 1 !!
+            addnumberDFSmethod1(res, nums, i+1, cur);            // i + 1 !!
             cur.pop_back();                              //回到上一個node  //[1,2]---> [1,3]
         }
     }
+    
+    void addnumberDFSmethod2(vector<vector<int>> &res, vector<int> &nums, int index, vector<int> cur){
+        
+        if(index == nums.size()) //跑完 做紀錄
+            res.push_back(cur);
+        else{
+            
+            //not chooose
+            addnumberDFSmethod2(res, nums, index+1, cur);
+            // choose
+            cur.push_back(nums[index]);
+            addnumberDFSmethod2(res, nums, index+1, cur);
+            cur.pop_back();
+            
+        }
+        
+    }
+    
 };
