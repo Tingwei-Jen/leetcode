a2683a3838cffaf46c02de81adf03c8dfd3efeed
Tingwei Jen <tingweijen@gmail.com>
2017-10-03 23:01:52 +0800

    Create 033_Search_in_Rotated_Sorted_Array.cpp

---
+++ leetcode/033_Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+     
+        if(nums.empty()) return -1;
+        int left = 0;
+        int right = nums.size()-1;
+        
+        while(left<right){
+            
+            int mid = left + (right-left)/2;    
+            if(nums[mid]==target) return mid;
+            if(nums[left] <= nums[mid]){                          //mid 在左半           
+                if(nums[left]<=target && nums[mid]>target)        //left target mid
+                    right = mid;
+                else                                              //left mid target
+                    left = mid+1;
+    
+            } else {                                              //mid 在右半
+                if(nums[right]>=target && nums[mid]<target)        //mid target right
+                    left = mid+1;
+                else                                              //target mid right
+                    right = mid;
+            }
+        }
+        
+        //離開迴圈後 left=right 重疊 
+        if(nums[left]==target) return left;
+        else return -1;
+    }
+};
