13740447d5efd763345d1266109a44245ba6f339
Tingwei Jen <tingweijen@gmail.com>
2017-10-06 15:07:19 +0800

    Update 026_Remove_Duplicates_from_Sorted_Array.cpp

--- leetcode/026_Remove_Duplicates_from_Sorted_Array.cpp
+++ leetcode/026_Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-
+        
         if(nums.size()==0)
             return 0;
         if(nums.size()==1)
             return 1;
         
-        int count = 0;
-        
-        for (int i=1;i<nums.size();i++){
+        int count=1;
+        for(int i=1;i<nums.size();i++){
             
-            if(nums[count] !=nums[i]){           // curent != next  看 下一個是不是等於現在這個
-                count++;                         // 如果不等於 --> count +1, 然後把nums[i] 放進來 把原先的取代掉
+            if(nums[i-1] != nums[i]){
                 nums[count] = nums[i];
-            }
+                count++;
+            }            
         }
-        
-        count = count +1;     //加上最一開始的數字
-        return count;        
+        return count;
     }
 };
