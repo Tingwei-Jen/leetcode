e73e55eda000ea206238a84c035d79acf951abed
Tingwei Jen <tingweijen@gmail.com>
2017-10-10 12:07:05 +0800

    Update 461_Hamming_Distance.cpp

--- leetcode/461_Hamming_Distance.cpp
+++ leetcode/461_Hamming_Distance.cpp
@@ -2,31 +2,20 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         
-        int n = x^y ; //XOR 
-            
-        bitset<32> b0(n);
-        cout<<"n:"<<(b0)<<endl;
-        cout<<"n>>1:"<<(b0>>1)<<endl;   //shift right 
-        
-        cout<<"test:"<<endl;
-        cout<<(5&1)<<endl;          //0101 & 0001 --> 0001    AND!!
-        cout<<(4&1)<<endl;          //0100 & 0001 --> 0000
-        cout<<(3&1)<<endl;
-        cout<<(2&1)<<endl;
-        cout<<(1&1)<<endl;
-        
+        int n = x^y ; //XOR               //XOR: 11--0
+                                          //     10--1
+                                          //     01--1
+                                          //     00--0
         int dis = 0;
-        for(int i=0;i<32;i++){
-            int k = (n>>i &1);
-            b0 = k;
-
-            if(k) {   //AND GATE 1&1 = 1  最後一位 是1時
-                dis++;
-                cout<<"i:"<<i<<endl;
-                cout<<"K: "<<b0<<endl;
-
-            }  
+        while(n>0){
+            if(n&1==1) dis++;           
+            n>>=1;
         }
         return dis;
     }
 };
+
+//ex. 4(0100) 1(0001) 4^1 = 0101
+//0101 & 0001 == 0001   dis++;     0101>>=1
+//0010 & 0001 == 0000 
+//0001 & 0001 == 0001   dis++;
